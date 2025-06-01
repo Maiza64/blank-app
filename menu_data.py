89a780c9de980dab50fa2ce4b2495be4fd762fea
Maiza64 <maizasakhi2005@gmail.com>
2025-06-01 17:43:59 +0700

    Update menu_data.py

--- menu_data.py
+++ menu_data.py
@@ -1,1 +1,32 @@
+menu_items = [
+    # Lauk-pauk
+    { "name": "Nasi", "price": 5000 },  # Tambahan nasi
+    { "name": "Ayam Goreng", "price": 8000 },
+    { "name": "Ayam Bakar", "price": 8000 },
+    { "name": "Telur Dadar", "price": 5000 },
+    { "name": "Telur Balado", "price": 5000 },
+    { "name": "Tempe Orek", "price": 5000 },
+    { "name": "Tempe Goreng", "price": 1000 },
+    { "name": "Tahu Bacem", "price": 5000 },
+    { "name": "Ikan Tongkol", "price": 5000 },
+    { "name": "Ikan Cue", "price": 5000},
+    { "name": "Perkedel Kentang", "price": 6000 },
+    { "name": "Daging Rawon", "price": 8000 },
+    { "name": "Ati Ampela Usus", "price": 7000 },
+    { "name": "Jamur Jengkol Tauge", "price": 5000 },
+    { "name": "Bakwan", "price": 1000 },
+    { "name": "Gulai Kikil;", "price": 5000 },
 
+    # Sayur
+    { "name": "Sayur Asem", "price": 3000 },
+    { "name": "Sayur Sop", "price":3000 },
+    { "name": "Sayur Lodeh", "price": 3000 },
+    { "name": "Tumis Kangkung", "price": 3000 },
+    { "name": "Capcay Kuah", "price": 3000 },
+
+    # Minuman
+    { "name": "Es Teh Manis", "price": 3000 },
+    { "name": "Es Jeruk", "price": 4000 },
+    { "name": "Teh Tawar Panas", "price": 2000 },
+    { "name": "Air Mineral", "price": 3000 }
+]
